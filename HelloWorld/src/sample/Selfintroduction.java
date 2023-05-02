efcbf5e5ce2537686365372a8b17f6f60441ada8
h.takahashi <h.takahashi.engineer@gmail.com>
2023-05-02 11:41:22 +0900

    Java1.3

---
+++ HelloWorld/src/sample/Selfintroduction.java
@@ -0,0 +1,24 @@
+package sample;
+
+public class Selfintroduction {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		//氏名
+		String name = "高橋広都";
+		//年齢
+		int age = 24;
+		//趣味
+		String hobby = "運動";
+		
+		/*
+		 自己紹介を出力する処理
+		*/
+		
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+	}
+
+}
